32edff11c473b4f14f5357a9f26af522f8c482f8
Jerome Hurley <vonswirl@googlemail.com>
2017-11-01 21:02:46 +0000

    nodemon dependency

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require('express');
 //Setup express
 const app = express();
 
-
 //Request Listener
-app.listen(4000, function(){
-console.log("now listen");
+app.listen(4000, function () {
+    console.log("now listen");
+});
+
+
+app.get('/api', function (req, res) {
+    console.log('GET request');
+    //res.end();
+    res.send({name: 'Paper'});
 });
