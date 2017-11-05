e45207702cbca4049b38d0c0c124fcae3715cff6
Jerome Hurley <vonswirl@googlemail.com>
2017-11-05 11:19:36 +0000

    mongoDB intialised, Catch JSON data error, settings adjusted, cmder intergrated cli

--- orderServiceController.js
+++ orderServiceController.js
@@ -1,12 +1,30 @@
+//Constraints below are dependencies required by this controller.
+const bodPars = require('body-parser');
 const express = require('express');
+const mong = require('mongoose');
 
-//Setup express
+//Sets up an instance of Express.js .
 const app = express();
 
-//Route setup for requests
+//Connection to Mongodb via mongoose
+mong.connect('mongodb://localhost/orders');
+
+//This overrides the depricated mongoose Promise with node.js Promise
+mong.Promise = global.Promise;
+
+//Allows Express to use body-parser tool to handle our JSON data.
+app.use(bodPars.json());
+
+//Allows Express access to order.js for HTTP verb functions. 
 app.use('/order', require('./orderRoute/order'));
+
+
+//Middleware error handler
+app.use(function (err, req, res, next) {
+    console.log(err);
+});
 
 //Request Listener
 app.listen(4000, function () {
-    console.log("now listen");
+    console.log("Response from orderServiceController");
 });
