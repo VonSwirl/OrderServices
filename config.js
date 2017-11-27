e1e1203f8b29864dbbce8a5cf1c29d474524371d
Jerome Hurley <vonswirl@googlemail.com>
2017-11-27 15:37:03 +0000

    commit

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ var config = {}
 config.databaseURL = 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service';
 config.orderServiceURLtoUpdateWithPurchase = "http://localhost:3001/purchasing/test"; 
 //"http://localhost:3004/PurchasingUpdate";
-//config.testDatabaseURL = "mongodb://localhost:27017/MyDb";
-config.AdminServicePurchaseURL = ":3001/purchasing/test";
-//config.stockServiceUpdaterURL= ":3001/purchasing/test";
-config.purchaseMissingStockURL = "http://theweefreeamigoz-papa.azurewebsites.net/purchasing/stockRequired";
+config.testDatabaseURL = "mongodb://localhost:27017/MyDb";
+config.AdminServicePurchaseURL = "http://localhost:3001/purchasing/test";
+config.stockServiceUpdaterURL= ":3001/purchasing/test";
+config.purchaseMissingStockURL = "http://localhost:3001/purchasing/stockRequired";
 
 module.exports = config;
